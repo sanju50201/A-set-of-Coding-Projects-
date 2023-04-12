5f89d33464b99b684076af66506ed548d492a48d
sanju50201 <sanjithk50201@gmail.com>
2023-04-12 17:05:26 +0530

    modified the word text guesser

--- Word Guesser/word_guess.py
+++ Word Guesser/word_guess.py
@@ -1,21 +1,34 @@
 import random
-
-name = input("What is your name? ")
-
-print("Good Luck ! ", name)
-
-words = ['rainbow', 'computer', 'science', 'programming',
-         'python', 'mathematics', 'player', 'condition',
-         'reverse', 'water', 'board', 'geeks', 'apple',]
-
-word = random.choice(words)
-
-print("Guess the characters")
+import sys
+import pyfiglet
 
 
-def guess_word():
-    guesses = ''
+def print_title():
+    ascii_banner = pyfiglet.figlet_format("Word Guesser")
+    print(ascii_banner)
 
+
+def load_words():
+    with open("/media/sanju/Programming/Python Projects/Word Guesser/words.txt", "r") as f:
+        words = f.read().splitlines()
+    return words
+
+
+def get_words():
+    words = load_words()
+    word = random.choice(words)
+    return word
+
+
+def prompt_guess():
+    guess = str(input("Guess a Character: "))
+    if guess == "quit":
+        sys.exit()
+    return guess
+
+
+def play_game(word):
+    guesses = set()
     turns = 10
 
     while turns > 0:
@@ -30,23 +43,34 @@ def guess_word():
         if failed == 0:
             print("You win")
             print(f"The word is {word}")
-            break
+            return
 
-        print("\n")
-        guess = input("guess a character:")
-        guesses += guess
+        print(f"\n\nYou have {turns} more guesses")
+        guess = prompt_guess()
+
+        try:
+            if guess in guesses:
+                raise ValueError("You already guessed that character")
+            elif len(guess) > 1:
+                raise ValueError("You can only guess one character at a time")
+            else:
+                guesses.add(guess)
+        except ValueError as error:
+            print(error)
+            continue
 
         if guess not in word:
             turns -= 1
+            print("Wrong guess")
 
-            print("Wrong")
-            print(f"You have {turns} more guesses")
-
-            if turns == 0:
-                print("You loose.\n")
+    print(f"You loose.\n The word was {word}")
 
 
-if __name__ == '__main__':
-    guess_word()
+if __name__ == "__main__":
+    print_title()
+    name = input("Enter your name: ")
+    print(f"Hello {name}, Welcome to Word Guesser")
+    word = get_words()
+    play_game(word)
 
 
